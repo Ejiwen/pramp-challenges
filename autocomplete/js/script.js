10f22dbfac1312fa5584f4233232f4437fd0a803
ejiwen <skibnewrim@gmail.com>
2020-10-05 21:05:40 -0700

    another way to solve the challenge

--- autocomplete/js/script.js
+++ autocomplete/js/script.js
@@ -1,36 +1,21 @@
 const inputElm = document.querySelector(".search-input");
 const listMovies = document.querySelector(".results");
-// console.log(inputElm);
 
-const searchMovieFn = async (e) => {
-    let text = e.target.value;
-    if(text.length > 0)
-    fetchData(text);
-    else
-    affichList([]);
-}
+inputElm.addEventListener("input", displayMovies);
 
-const fetchData = async (dataToSearch) => {
-
-    const data = await fetch('./js/moviesData.json');
-    const d = await data.json();
-
-    const dataReg = new RegExp(`^${dataToSearch}`, 'gi');
-    let dataBack = d.filter(film => film.title.match(dataReg));
-
-    affichList(dataBack);
-
-}
-
-
-function affichList(elements) {
-    listMovies.innerHTML = '';
-    elements.map( el => {
-        const liElm = document.createElement('li');
-        liElm.innerHTML = el.title;
-        listMovies.appendChild(liElm);
+async function displayMovies(e){
+ let movieTitle = e.target.value;
+ let dataMovies = await searchData(movieTitle)
+ 
+ if(movieTitle.length > 0) {
+    listMovies.innerHTML = "";
+    dataMovies.map( film => {
+        let el = document.createElement("li");
+        el.innerHTML = film.title;
+        listMovies.appendChild(el);
     })
-   
+ } else {
+     listMovies.innerHTML = "";
+ }
+ 
 }
-
-inputElm.addEventListener('input', searchMovieFn);
